8d6da6d20809600c6be3a2dceababd7ca54a170a
kavic <kavicse.23@gmail.com>
2023-01-23 08:45:04 -0600

    models are updated

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongTo(Catergory, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
 // Categories have many Products
+Catergory.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  through: {
+    model: 'ProductTag',
+    unique: false
+  },
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongToMany(Product, {
+  throught: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id'
+});
 module.exports = {
   Product,
   Category,
