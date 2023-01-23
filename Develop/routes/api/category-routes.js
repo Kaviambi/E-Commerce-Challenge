1574e7d467ab75309306d4138bc5a3cae758f54d
kavic <kavicse.23@gmail.com>
2023-01-23 14:24:47 -0600

    final check in insomnia and heroku

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -13,6 +13,7 @@ router.get('/', async (req, res) => {
   });
   if(!categoryData){
     res.status(404).json({message: "Category cannot be found!"});
+
   }
   res.status(200).json(categoryData);
  } catch(err) {
@@ -29,6 +30,7 @@ router.get('/:id', async (req, res) => {
   });
   if(!categoryData){
     res.status(404).json({ message: "No Category found with this ID!"});
+    return;
   }
   res.status(200).json(categoryData);
  } catch (err){
@@ -42,7 +44,7 @@ try {
   const categoryData = await Category.create(req.body);
   res.status(200).json(categoryData);
 } catch (err) {
-  res.status(400).json (err);
+  res.status(500).json (err);
 }
 });
 
@@ -74,7 +76,6 @@ router.delete('/:id', async(req, res) => {
     });
     if(!categoryData) {
       res.status(404).json({ message: "No Category found with this id"});
-      return;
     }
     res.status(200).json(categoryData);
   } catch (err) {
